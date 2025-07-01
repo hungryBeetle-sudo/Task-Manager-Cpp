05cd9726b523769b0381fae6ba576ae96ae1f7ab
hungryBeetle-sudo <erik.ts@outlook.com>
2025-06-30 23:39:29 -0300

    Create initial file handling logic.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(){
-	std::cout << "Hello, World!";
+	std::fstream MyFile("task-list.txt", std::fstream::in | std::fstream::out);
+
+	std::string text_fragment;
+	std::string file_full_text;
+
+	while( std::getline(MyFile, text_fragment) )
+		file_full_text.append(text_fragment + "\n");
+
+	std::cout << file_full_text;
+
+        MyFile.close();
+
+	return 0;
 }
